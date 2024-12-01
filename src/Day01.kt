2fa3c6a2b14025f0b760ca76e1c2e81c674eefba
Ali Shirzad <techprdblog@gmail.com>
2024-12-01 22:28:58 +1000

    Day 01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftList = getLeft(input)
+        val rightList = getRight(input)
+
+        return leftList.zip(rightList).sumOf {
+            if (it.first - it.second > 0) {
+                it.first - it.second
+            } else {
+                it.second - it.first
+            }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val leftList = getLeft(input)
+        val rightList = getRight(input)
+
+        return leftList.sumOf { left ->
+            var similarity = 0
+            rightList.forEach { right ->
+                if (left == right) {
+                    similarity++
+                }
+            }
+            left * similarity
+        }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun getRight(lines: List<String>): List<Int> {
+    val rightList = lines.map {
+        // last 5 characters
+        val lastFive = it.substring(8, 13)
+        lastFive.toInt()
+    }.sorted()
+    return rightList
+}
+
+private fun getLeft(lines: List<String>): List<Int> {
+    val leftList = lines.map {
+        // first 5 characters
+        val firstFive = it.substring(0, 5)
+        firstFive.toInt()
+    }.sorted()
+    return leftList
 }
